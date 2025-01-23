513369bfd1990d9efa42ec0524ce3650e05c4b35
am.zhuravlev <am.zhuravlev@NTB44766>
2025-01-23 14:53:27 +0300

    Тоже промежуточный вариант но с базовой агрегацией от оцелот (важно чтобы были видны свагеры других сервисов)

--- OcelotAggregate.ApiGateway/Program.cs
+++ OcelotAggregate.ApiGateway/Program.cs
@@ -8,22 +8,28 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Configuration
-    //.AddOcelot(builder.Environment)
-    .AddJsonFile("ocelot.json")
+   // .AddOcelot(builder.Environment)
+   .AddJsonFile("ocelot.json")
+   //.AddJsonFile("ocelot.swagger.json")
+  //  .AddJsonFile("ocelot.aggregates.json")
+  //  .AddJsonFile("ocelot3.aggregates2.json")
    .AddEnvironmentVariables();
 
 builder.Services.AddOcelot(builder.Configuration);
-builder.Services.AddSwaggerForOcelot(builder.Configuration, o => { });
+builder.Services.AddSwaggerForOcelot(builder.Configuration, o =>
+{
+    o.GenerateDocsForAggregates = true;
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {
     app.UseSwagger();
     app.UseSwaggerForOcelotUI(opt => { });
     //app.UseSwaggerUI();
-}
+//}
 
 
 app.UseOcelot().Wait();
